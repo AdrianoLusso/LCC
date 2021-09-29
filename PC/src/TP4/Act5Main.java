11a2fae1cccd39ce5c73961ff1039bf6773d5ffc
Adriano Lusso <lussoadriano@gmail.com>
2021-09-29 14:50:06 -0300

    error en variable c

--- PC/src/TP4/Act5Main.java
+++ PC/src/TP4/Act5Main.java
@@ -22,46 +22,45 @@ public class Act5Main {
     }
 
     /*
-    Fallo:
- 1 genera su trabajo.
+    1 genera su trabajo.
 9 genera su trabajo.
 8 genera su trabajo.
 7 genera su trabajo.
 6 genera su trabajo.
 5 genera su trabajo.
 3 genera su trabajo.
-4 genera su trabajo.
 0 genera su trabajo.
+4 genera su trabajo.
 2 genera su trabajo.
-1 c: 3
-8 c: 2
-1 esta imprimiendo en imp A
-9 c: 1
-8 esta imprimiendo en imp B
-6 c: 0
-9 esta imprimiendo en imp B
-7 c: 0
-5 c: 2
-5 esta imprimiendo en imp A
-4 c: 1
-3 c: 1!!!ACA ESTA EL ERROR
-0 c: 1
-2 c: 1
-4 esta imprimiendo en imp A
-1 termino
-3 esta imprimiendo en imp A
-8 termino
+5 c: 3
+6 c: 2
+3 c: 3
 6 esta imprimiendo en imp B
-9 termino
-7 esta imprimiendo en imp B
-5 termino
+8 c: 1
+0 c: 2
+3 esta imprimiendo en imp A
 0 esta imprimiendo en imp A
-4 termino
-2 esta imprimiendo en imp A
-3 termino
-7 termino
-6 termino
+4 c: 1
+4 esta imprimiendo en imp A
+9 c: 0
+8 esta imprimiendo en imp B
+7 c: 0
+1 c: 0
+5 esta imprimiendo en imp A
+2 c: -1 !!!!!Fallo
 0 termino
+4 termino
+6 termino
+3 termino
+9 esta imprimiendo en imp B
+1 esta imprimiendo en imp A
+2 esta imprimiendo en imp A
+8 termino
+5 termino
+7 esta imprimiendo en imp B
 2 termino
+1 termino
+9 termino
+7 termino
     */
 }
